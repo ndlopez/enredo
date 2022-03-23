33ffdf3e30a9e59e7403674e00f8b94693886b2d
Diego Lopez <lopez_diego@protonmail.com>
2022-03-23 10:33:20 +0900

    Revised CSS and JS

--- static/shuffle_words.js
+++ static/shuffle_words.js
@@ -11,17 +11,43 @@ String.prototype.shuffle = function () {
     }
     return a.join("");
 }
+var text="";
+var jumble_words=[];
+var xhr=new XMLHttpRequest();
+var my_data="https://raw.githubusercontent.com/ndlopez/jumble_game/main/static/jumble_words.json";
+xhr.onreadystatechange=function(){
+    try {
+        if(this.readyState ==4 && this.status == 200){
+            jumble_words = JSON.parse(this.responseText);
+            //console.log(jumble_words);
+            for (let jdx = 0; jdx < jumble_words.length; jdx++) {
+                var jumble_word=jumble_words[jdx];
+                text += "<div class='row' id='shufWord'><h2>" + jumble_word.shuffle() + "</h2></div>";
+                //console.log("jumble word",jumble_word,text);
+                //document.getElementById("jumble_this_words").innerHTML = text;
+                /* Build as many inputs as the length of a word*/
+                text += "<div class='row' id='inputLetters'>";
+                for (let idx = 0; idx < jumble_word.length; idx++) {
+                    text += "<div class='oneLetterCol'><input></input></div>";
+                }
+                text += "</div>";
+                console.log("inputs",text);
+                document.getElementById("jumble_this_words").innerHTML = text;
+            }
+        }
+        
+    } catch (error) {
+        console.log("Error connecting to",my_data);
+    }
+};
+xhr.open("GET",my_data,true);
+xhr.send();
+console.log("my words",jumble_words.type);
+/*const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const myObj = JSON.parse(this.responseText);
+  document.getElementById("demo").innerHTML = myObj.name;
+};
+xmlhttp.open("GET", "json_demo.txt");
+xmlhttp.send();*/
 
-var jumble_word="output";
-//var text="";
-var text = "<h2>" + jumble_word.shuffle() + "</h2>";
-console.log("jumble word",jumble_word,text);
-document.getElementById("shufWord").innerHTML = text;
-/* Build as many inputs as the length of a word*/
-text="";
-for (let idx = 0; idx < jumble_word.length; idx++) {
-    //const element = jumble_word[idx];
-    text += "<div class='oneLetterCol'><input></input></div>";
-}
-console.log("inputs",text);
-document.getElementById("inputLetters").innerHTML = text;
